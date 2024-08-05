6d1b561a8e18dfe81aef8da48944d0ee9569e854
VLADISLAW9 <duplinskij_v_d@mail.ru>
2024-08-05 16:03:01 +0700

    add logout module, add suspense query for get session data

--- src/shared/components/Layout/components/Header/hooks/useHeader.ts
+++ src/shared/components/Layout/components/Header/hooks/useHeader.ts
@@ -1,3 +1,6 @@
+import { useEffect, useState } from 'react';
+import { useApolloClient } from '@apollo/client';
+import { LOCAL_STORAGE } from '@constants/localStorage';
 import { useModal } from '@hooks/useModal';
 
 import { useGetSessionSuspenseQuery } from '@/shared/graphql/__generated__';
@@ -5,18 +8,32 @@ import { useGetSessionSuspenseQuery } from '@/shared/graphql/__generated__';
 import { getNavItemsList } from '../selectors/getNavItems';
 
 export const useHeader = () => {
-  const sessionQuery = useGetSessionSuspenseQuery();
+  const client = useApolloClient();
 
-  const isLoggedIn = !!sessionQuery.data?.session.user;
+  const sessionQuery = useGetSessionSuspenseQuery({
+    errorPolicy: 'ignore'
+  });
+
+  const [isLoggedIn, setIsLoggedIn] = useState(!!sessionQuery.data?.session?.user);
 
   const [isOpenAuthModal, { open: openAuthModal, close: closeAuthModal }] = useModal();
-
   const navItemsList = getNavItemsList(isLoggedIn);
 
-  const onLogout = () => {};
+  const onLogout = async () => {
+    await client.clearStore();
+    localStorage.removeItem(LOCAL_STORAGE.AUTH_TOKEN);
+    setIsLoggedIn(false);
+  };
   const onLogin = () => {
     openAuthModal();
   };
+
+  useEffect(() => {
+    if (sessionQuery.data?.session?.user) {
+      closeAuthModal();
+      setIsLoggedIn(true);
+    }
+  }, [sessionQuery.data]);
 
   return {
     state: {
